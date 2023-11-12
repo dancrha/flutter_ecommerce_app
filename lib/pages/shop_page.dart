ba098df6c58e765819f68ee86b12a0e6e86c944b
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-11-12 15:15:35 -0500

    progress

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,80 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({Key? key}) : super(key: key);
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text('Search'),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+
+        // Message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(
+            'Everyone flies...some fly longer than others',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+        ),
+
+        // Hot picks
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(
+                'See all',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+              )
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        // Expanded and ListView
+        Expanded(
+          child: ListView.builder(
+            itemCount: 10, // Replace with your actual item count
+            itemBuilder: (context, index) {
+              return ShoeTile(); // Replace with your actual item widget
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
